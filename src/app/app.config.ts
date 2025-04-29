1bb48a2de397cbb1cb3c6869574e455810be4c5e
sandip <dassandip@190gmail.com>
2025-04-29 20:14:51 +0530

    Revert "cors policy"
    This reverts commit 586c1290caed4e095ef2f227b7ed19c554e08053.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 import { DatePipe } from "@angular/common";
-import { corsInterceptor  } from './cors.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),provideClientHydration(),provideAnimations(),provideHttpClient(withFetch(),withInterceptors([corsInterceptor ])),provideToastr({timeOut: 10000,positionClass: 'toast-top-right',preventDuplicates: true,}),DatePipe]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(),provideHttpClient(withFetch()),provideToastr({ timeOut: 10000, positionClass: 'toast-top-right', preventDuplicates: true,}),DatePipe]
 };
