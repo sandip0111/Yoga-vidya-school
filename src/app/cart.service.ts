d3df25be8f974763d14b6b689b2a57741f2cc77f
palash-1998 <palashbiswas.1998@gmail.com>
2025-05-11 16:04:53 +0530

    Modified Cart count item function

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -50,7 +50,8 @@ export class CartService {
     const existingItem = this.items.find(i => i.id === item.id);
 
     if (existingItem) {
-      existingItem.quantity += item.quantity;
+      // existingItem.quantity += item.quantity;
+      existingItem.quantity = item.quantity;
     } else {
       this.items.push(item);
     }
