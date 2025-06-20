0628612435df29dd976cecce5ea52b41b0f424fb
Kaushik_Das <kaushik@sentientgeeks.com>
2025-06-20 21:18:03 +0530

    change

--- src/app/enum/payment.ts
+++ src/app/enum/payment.ts
@@ -1,18 +1,18 @@
 export enum paymentkey {
-  stripeKey = 'pk_live_51LJJXISEQq0H4GuE8KMgQV23uQA21MqJLP8XsL3fNZBpwRmX9n8VK4CdcBbeSNbnptLCNn7SScrNvIERlhyKsO1c00ILj5f3hP',
-  // stripeKey = 'pk_test_51LTjKYSDnZBoIVm7pF6anOLQhi4oPrvRNYuOP0fF0wOptRzE1m0QqtvAOo1wi6VUVb5cMgThi8FGGeSUhZ10KRIW00zlCy2Ff0',
+  // stripeKey = 'pk_live_51LJJXISEQq0H4GuE8KMgQV23uQA21MqJLP8XsL3fNZBpwRmX9n8VK4CdcBbeSNbnptLCNn7SScrNvIERlhyKsO1c00ILj5f3hP',
+  stripeKey = 'pk_test_51LTjKYSDnZBoIVm7pF6anOLQhi4oPrvRNYuOP0fF0wOptRzE1m0QqtvAOo1wi6VUVb5cMgThi8FGGeSUhZ10KRIW00zlCy2Ff0',
 }
 export enum stripePaymentKey {
-  basicInr = 'price_1QmsTUSEQq0H4GuEZfWd5UJu',
-  // basicInr = 'price_1NI7hnSEQq0H4GuEWS5qIffa',
+  // basicInr = 'price_1QmsTUSEQq0H4GuEZfWd5UJu',
+  basicInr = 'price_1NI7hnSEQq0H4GuEWS5qIffa',
   standardInr = 'price_1NI6oxSEQq0H4GuERpBbilF2',
   premiumInr = 'price_1NI6oxSEQq0H4GuEx9fdhEd0',
   basicUsd = 'price_1QmychSEQq0H4GuEAipCDoPU',
   basicEur = 'price_1Qq8OGSEQq0H4GuExvBjijrv',
   standardUsd = 'price_1NIRatSEQq0H4GuE0DOlcCNa',
   premiumUsd = 'price_1NIRbNSEQq0H4GuEeLvnyPu2',
-  discountInr = 'price_1RaKE2SEQq0H4GuE3smtGoFk',
-  // discountInr = 'price_1RaKHwSEQq0H4GuE2wxFlMLD',
+  // discountInr = 'price_1RaKE2SEQq0H4GuE3smtGoFk',
+  discountInr = 'price_1RaKHwSEQq0H4GuE2wxFlMLD',
   discountUsd = 'price_1RaKEeSEQq0H4GuEU8cZgk7Q',
   discountEur = 'price_1RaKFISEQq0H4GuEzeks9Aep',
 }
