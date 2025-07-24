b18619f8ec3118ac7583582367b6f2a0ece38985
Kaushik Das <kaushik.mca.18@gmail.com>
2025-07-25 01:27:00 +0530

    Add food image to s3Bucket enum; enhance services layout with grid display and responsive styles in CSS; update navbar structure for improved user experience.

--- src/app/enum/s3Bucket.ts
+++ src/app/enum/s3Bucket.ts
@@ -9,6 +9,7 @@ export enum s3Bucket {
   room3 = 'https://my-s3-images-bucket.s3.amazonaws.com/images/acc2_zwdgpr.jpg',
   room4 = 'https://my-s3-images-bucket.s3.amazonaws.com/images/acc1_e2fcux.jpg',
   roomMain = 'https://my-s3-images-bucket.s3.amazonaws.com/images/IMG_6828_cchusm.jpg',
+  food = "https://my-s3-images-bucket.s3.amazonaws.com/images/food_dwn1yl.jpg"
 }
 export enum youtubeLink {
   trainingImportance = "https://www.youtube.com/embed/t7l1cqDj1tA?si=ZJCQXJYDLzDwLLvm"
