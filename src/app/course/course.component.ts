b04f898d15305f9dd0264c1aef8eab95c31704fe
Kaushik Das <kaushik.mca.18@gmail.com>
2025-08-19 23:14:46 +0530

    online pages

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,8 +1,19 @@
-import { Component,Inject, Renderer2, ElementRef, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  Inject,
+  Renderer2,
+  ElementRef,
+  ViewEncapsulation,
+} from '@angular/core';
 import { CommonModule, DOCUMENT } from '@angular/common';
 import { WebapiService } from '../webapi.service';
-import { Router,ActivatedRoute } from '@angular/router';
-import { DomSanitizer,Title,Meta,SafeResourceUrl } from '@angular/platform-browser';
+import { Router, ActivatedRoute } from '@angular/router';
+import {
+  DomSanitizer,
+  Title,
+  Meta,
+  SafeResourceUrl,
+} from '@angular/platform-browser';
 import { BottomNavComponent } from '../includes/home/bottom-nav/bottom-nav.component';
 import { AboutComponent } from '../includes/home/about/about.component';
 import { JoinTrainingComponent } from '../includes/home/join-training/join-training.component';
@@ -17,8 +28,8 @@ import { TestimonialsComponent } from '../includes/home/testimonials/testimonial
 import { FaqComponent } from '../includes/home/faq/faq.component';
 import { CourseMentorComponent } from './course-mentor/course-mentor.component';
 import { SafePipe } from '../safe.pipe';
-import { VideoReviewsComponent } from "./video-reviews/video-reviews.component";
-import {ReviewListComponentComponent} from "../text-review-list/review-list-component/review-list-component.component";
+import { VideoReviewsComponent } from './video-reviews/video-reviews.component';
+import { ReviewListComponentComponent } from '../text-review-list/review-list-component/review-list-component.component';
 
 @Component({
   selector: 'app-course',
@@ -41,94 +52,106 @@ import {ReviewListComponentComponent} from "../text-review-list/review-list-comp
     CourseMentorComponent,
     SafePipe,
     VideoReviewsComponent,
-    ReviewListComponentComponent
-],
+    ReviewListComponentComponent,
+  ],
   templateUrl: './course.component.html',
-  styleUrl: './course.component.css'
+  styleUrl: './course.component.css',
 })
 export class CourseComponent {
-  public defaultImageLandscape = 'https://my-s3-images-bucket.s3.amazonaws.com/images/deafult_banner_ockzmy.jpg';
+  public defaultImageLandscape =
+    'https://my-s3-images-bucket.s3.amazonaws.com/images/deafult_banner_ockzmy.jpg';
   coreValues = [
     {
       image: 'bullet.png',
       title: 'Authenticity',
       title1: '',
-      text: 'Discover traditional yoga practices as mentioned in our ancient yoga texts, our experienced teachers don’t compromise with the authenticity but make the complex topics easy in accessible way.'
+      text: 'Discover traditional yoga practices as mentioned in our ancient yoga texts, our experienced teachers don’t compromise with the authenticity but make the complex topics easy in accessible way.',
     },
     {
       image: 'bullet.png',
       title: 'Holistic',
       title1: 'Wellness',
-      text: 'In the online yoga classes, we focus on total wellness through Movements, Breath and Meditation.'
+      text: 'In the online yoga classes, we focus on total wellness through Movements, Breath and Meditation.',
     },
     {
       image: 'bullet.png',
       title: 'Inclusivity',
       title1: '',
-      text: 'With more than 4 years of experience in online teaching, we offer accessible classes suitable for all levels, from beginners to advanced practitioners'
+      text: 'With more than 4 years of experience in online teaching, we offer accessible classes suitable for all levels, from beginners to advanced practitioners',
     },
     {
       image: 'bullet.png',
       title: 'Empowering',
       title1: '',
-      text: 'At Yoga Vidya School, Rishikesh, we are committed to empower our students and community by offering  lifelong assistance and guidance'
+      text: 'At Yoga Vidya School, Rishikesh, we are committed to empower our students and community by offering  lifelong assistance and guidance',
     },
     {
       image: 'bullet.png',
       title: 'High ',
       title1: 'Standards',
-      text: 'We are backed with the best yoga teachers of India and thus never compromise with the standards and quality of our classes.'
-    }
+      text: 'We are backed with the best yoga teachers of India and thus never compromise with the standards and quality of our classes.',
+    },
   ];
-  slug: any='';
-  imageUrl:any='';
+  slug: string | undefined = '';
+  imageUrl: string = '';
   courseList: any;
-  introLink:any='';
+  introLink: string = '';
 
-
-  constructor(private webapiService: WebapiService, private _activatedRoute: ActivatedRoute, private router: Router, private title: Title, private meta: Meta, protected sanitizer: DomSanitizer,private _renderer2: Renderer2,@Inject(DOCUMENT) private _document: Document){
-
+  constructor(
+    private webapiService: WebapiService,
+    private _activatedRoute: ActivatedRoute,
+    private router: Router,
+    private title: Title,
+    private meta: Meta,
+    protected sanitizer: DomSanitizer,
+    private _renderer2: Renderer2,
+    @Inject(DOCUMENT) private _document: Document
+  ) {
     this.slug = this._activatedRoute.snapshot.routeConfig?.path;
     this.imageUrl = this.webapiService.imageUrl;
-    if(this.slug){
+    if (this.slug) {
       this.getCourseBySlug(this.slug);
     }
-    }
-    ngOnInit():void{
-      const canonicalUrl = 'https://www.yogavidyaschool.com' + this.router.url;
-      const link = this._document.querySelector('link[rel="canonical"]');
-      this._renderer2.setAttribute(link, 'href', canonicalUrl);
-    }
+  }
+  ngOnInit(): void {
+    const canonicalUrl = 'https://www.yogavidyaschool.com' + this.router.url;
+    const link = this._document.querySelector('link[rel="canonical"]');
+    this._renderer2.setAttribute(link, 'href', canonicalUrl);
+  }
 
-    getCourseBySlug(slug: any) {
-      let data = {
-        "slug": slug
+  getCourseBySlug(slug: any) {
+    let data = {
+      slug: slug,
+    };
+    this.webapiService.getCourseById(data).subscribe((res: any) => {
+      // console.log(res.data, 'course Data');
+      const currentDate = new Date();
+      if (res.data.length > 0) {
+        this.courseList = res.data[0];
+        // this.courseName = res.data[0].coursetitle;
+        // const currentDate = new Date();
+        // this.upcomingDates = res.data[0]?.upcomingEventInfo.filter((item: any) => {
+        //   const itemDate = new Date(item.startDate);
+        //   return itemDate >= currentDate;
+        // });
+        this.introLink =
+          'https://www.youtube.com/embed/' + res.data[0].courseintrovideoId;
+        //console.log(this.introLink,'--');
+
+        this.title.setTitle(res.data[0].metaTitle);
+        this.meta.updateTag({
+          name: 'keywords',
+          content: res.data[0].metaKeyword,
+        });
+        this.meta.updateTag({
+          name: 'description',
+          content: res.data[0].metaDescription,
+        });
+        // this.checkForCourse(this.userId, res.data[0]._id);
+      } else {
+        // this.router.navigate(['/']);
+        // this.spinner.hide();
       }
-      this.webapiService.getCourseById(data).subscribe((res: any) => {
-        // console.log(res.data, 'course Data');
-        const currentDate = new Date();
-        if (res.data.length > 0) {
-
-          this.courseList = res.data[0];
-          // this.courseName = res.data[0].coursetitle;
-          // const currentDate = new Date();
-          // this.upcomingDates = res.data[0]?.upcomingEventInfo.filter((item: any) => {
-          //   const itemDate = new Date(item.startDate);
-          //   return itemDate >= currentDate;
-          // });
-          this.introLink = 'https://www.youtube.com/embed/'+ res.data[0].courseintrovideoId;
-           //console.log(this.introLink,'--');
-
-          this.title.setTitle(res.data[0].metaTitle);
-          this.meta.updateTag({ name: 'keywords', content: res.data[0].metaKeyword });
-          this.meta.updateTag({ name: 'description', content: res.data[0].metaDescription });
-          // this.checkForCourse(this.userId, res.data[0]._id);
-        }
-        else {
-          // this.router.navigate(['/']);
-          // this.spinner.hide();
-        }
-      })
-    }
-
+    });
+  }
 }
