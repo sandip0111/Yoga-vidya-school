dcd97979f0878d11e8f75b80661d476343d09588
sandip <dassandip@190gmail.com>
2025-09-06 18:00:30 +0530

    remove class anuj

--- src/app/course/course-mentor/course-mentor.component.ts
+++ src/app/course/course-mentor/course-mentor.component.ts
@@ -179,23 +179,23 @@ export let jsonData = [
       'Interactive class combining Hatha asanas and pranayama each morning for holistic physical, mental, and spiritual growth. Suitable for all levels, with focus on correct alignment and routine building.',
     url: 'prashant-jhakmola-online-class',
   },
-  {
-    id: 2,
-    name: 'Anuj Pareek',
-    image: s3Bucket.courseMentor2,
-    title: 'Therapeutic Hatha Yoga',
-    weeklyTime: 'From Monday to Thursday',
-    time: {
-      time1: '5:30 am',
-      time2: '6:30 am',
-      stamp: 'IST',
-    },
-    nextBatch: 'October the 6th',
-    price: { priceInIndian: 1999, priceInUSD: 50 },
-    description:
-      'Progressive, dynamic and therapeutic Hatha sequence tailored for mobility and release of long-held tension from sedentaries lifestyles.',
-    url: 'anuj-online-class',
-  },
+  // {
+  //   id: 2,
+  //   name: 'Anuj Pareek',
+  //   image: s3Bucket.courseMentor2,
+  //   title: 'Therapeutic Hatha Yoga',
+  //   weeklyTime: 'From Monday to Thursday',
+  //   time: {
+  //     time1: '5:30 am',
+  //     time2: '6:30 am',
+  //     stamp: 'IST',
+  //   },
+  //   nextBatch: 'October the 6th',
+  //   price: { priceInIndian: 1999, priceInUSD: 50 },
+  //   description:
+  //     'Progressive, dynamic and therapeutic Hatha sequence tailored for mobility and release of long-held tension from sedentaries lifestyles.',
+  //   url: 'anuj-online-class',
+  // },
   {
     id: 3,
     name: 'Taniya Verma',
@@ -207,28 +207,28 @@ export let jsonData = [
       time2: '6:00 PM',
       stamp: 'IST',
     },
-    nextBatch: 'October the 6th. (Not available during September)',
+    nextBatch: 'Coming soon....',
     price: { priceInIndian: 1999, priceInUSD: 40 },
     description:
       'A gentle and supportive practice designed specially for women from menstruation to menopause combining asana, pranayama, nutrition tips and hormone-balancing restorative techniques.',
     url: 'taniya-verma-online-class',
   },
-  {
-    id: 4,
-    name: 'Anuj Pareek',
-    image: s3Bucket.courseMentor2,
-    title: 'Intermediate Alignment Based Class',
-    weeklyTime: 'Monday / Wednesday / Friday',
-    time: {
-      time1: '6:15 pm',
-      time2: '7:15 pm',
-      stamp: 'IST',
-    },
-    nextBatch: 'October the 6th',
-    price: { priceInIndian: 3500, priceInUSD: 70 },
-    description:
-      'Advanced, mobility-rich sessions emphasizing arm balance and inversions with therapeutic methods. Ideal for progressing students beyond basic.',
-    url: 'anuj-online-class',
-  }
+  // {
+  //   id: 4,
+  //   name: 'Anuj Pareek',
+  //   image: s3Bucket.courseMentor2,
+  //   title: 'Intermediate Alignment Based Class',
+  //   weeklyTime: 'Monday / Wednesday / Friday',
+  //   time: {
+  //     time1: '6:15 pm',
+  //     time2: '7:15 pm',
+  //     stamp: 'IST',
+  //   },
+  //   nextBatch: 'October the 6th',
+  //   price: { priceInIndian: 3500, priceInUSD: 70 },
+  //   description:
+  //     'Advanced, mobility-rich sessions emphasizing arm balance and inversions with therapeutic methods. Ideal for progressing students beyond basic.',
+  //   url: 'anuj-online-class',
+  // }
   
 ];
