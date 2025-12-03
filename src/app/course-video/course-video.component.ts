e1b573712d76fe9bdba77dfe8f5fa015e3ab0195
kingshukmath2014 <kingshukmath2014@gmail.com>
2025-12-03 21:40:38 +0530

    feat: implement course video playback component with feedback forms and PDF download functionality.

--- src/app/course-video/course-video.component.ts
+++ src/app/course-video/course-video.component.ts
@@ -729,8 +729,8 @@ export class CourseVideoComponent {
           });
       }
     }, 4000);
-    if(this.slug != routeEnum['200TTC']){
-     this.getAccessLog(this.userId, id);
+    if (this.slug != routeEnum['200TTC']) {
+      this.getAccessLog(this.userId, id);
     }
   }
   onlineVideoLoad(obj: onLineVideoModel) {
@@ -766,5 +766,18 @@ export class CourseVideoComponent {
         );
       }
     }, 4000);
+  }
+
+  downloadPdf() {
+    const pdfUrl = 'assets/BREATH DETOX YOGA.pdf';
+    const fileName = 'BREATH_DETOX_YOGA.pdf';
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = fileName;
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
 }
