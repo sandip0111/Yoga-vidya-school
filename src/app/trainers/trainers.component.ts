b034cf609b6d5b8624fb1dc33b5156e3682f59b9
kingshukmath2014 <kingshukmath2014@gmail.com>
2026-02-04 20:56:48 +0530

    feat: add TrainersComponent to display mentor profiles with dedicated styling and SEO.

--- src/app/trainers/trainers.component.ts
+++ src/app/trainers/trainers.component.ts
@@ -1,9 +1,9 @@
-import { Component,Renderer2,Inject } from '@angular/core';
+import { Component, Renderer2, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { WebapiService } from '../webapi.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Router } from '@angular/router';
-import { Title,Meta } from '@angular/platform-browser';
+import { Title, Meta } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
@@ -11,17 +11,25 @@ import { LazyLoadImageModule } from 'ng-lazyload-image';
 @Component({
   selector: 'app-trainers',
   standalone: true,
-  imports: [CommonModule,RouterLink,LazyLoadImageModule],
+  imports: [CommonModule, RouterLink, LazyLoadImageModule],
   templateUrl: './trainers.component.html',
-  styleUrl: './trainers.component.css'
+  styleUrl: './trainers.component.css',
 })
 export class TrainersComponent {
+  public defaultImage =
+    'https://miro.medium.com/max/441/1*9EBHIOzhE1XfMYoKz1JcsQ.gif';
+  mentorList: any;
+  imageUrl: any;
 
-  public defaultImage = 'https://miro.medium.com/max/441/1*9EBHIOzhE1XfMYoKz1JcsQ.gif';
-  mentorList: any
-  imageUrl: any
-
-  constructor(private webapiService: WebapiService, private spinner: NgxSpinnerService, private title: Title, private meta: Meta, private router: Router, @Inject(DOCUMENT) private _document: Document, private _renderer2: Renderer2) { }
+  constructor(
+    private webapiService: WebapiService,
+    private spinner: NgxSpinnerService,
+    private title: Title,
+    private meta: Meta,
+    private router: Router,
+    @Inject(DOCUMENT) private _document: Document,
+    private _renderer2: Renderer2,
+  ) {}
 
   ngOnInit(): void {
     this.spinner.show();
@@ -29,12 +37,19 @@ export class TrainersComponent {
     this.getAllMentors();
     setTimeout(() => {
       this.title.setTitle('Mentors');
-      this.meta.updateTag({ name: 'keywords', content: 'Yoga Vidya School Rishikesh Contact Details' });
-      this.meta.updateTag({ name: 'description', content: 'We would love to hear from you. Whether you want more information on our classes or want to attend any events that we hold, just give us a quick call.' });
+      this.meta.updateTag({
+        name: 'keywords',
+        content: 'Yoga Vidya School Rishikesh Contact Details',
+      });
+      this.meta.updateTag({
+        name: 'description',
+        content:
+          'We would love to hear from you. Whether you want more information on our classes or want to attend any events that we hold, just give us a quick call.',
+      });
       const canonicalUrl = 'https://www.yogavidyaschool.com' + this.router.url;
       const link = this._document.querySelector('link[rel="canonical"]');
       this._renderer2.setAttribute(link, 'href', canonicalUrl);
-    }, 1000)
+    }, 1000);
   }
 
   getAllMentors() {
@@ -43,5 +58,4 @@ export class TrainersComponent {
       this.spinner.hide();
     });
   }
-
 }
