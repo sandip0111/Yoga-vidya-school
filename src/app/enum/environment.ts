149a38474db0babbd2a0afd170ece190957e4a15
Kaushik <kaushik.das.mca18@gmail.com>
2026-05-07 18:20:21 +0530

    image link change

--- src/app/enum/environment.ts
+++ src/app/enum/environment.ts
@@ -1,6 +1,6 @@
 export enum environment {
   apiUrl = 'https://yogavidyaschool.com:3000/',
-  imageUrl = 'https://d3mzqk1fxuwngx.cloudfront.net/images/',
+  imageUrl = 'https://d29rwrqvux6m5p.cloudfront.net/images/',
   // apiUrl = 'http://localhost:3000/',
   // imageUrl = 'http://localhost:3000/public/images/',
 }
