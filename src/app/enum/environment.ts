602d9db52df6d8f27800c51459d18a990f5917ca
kingshukmath2014 <kingshukmath2014@gmail.com>
2026-05-10 19:43:48 +0530

    feat: add environment enum for API and image base URLs

--- src/app/enum/environment.ts
+++ src/app/enum/environment.ts
@@ -1,5 +1,5 @@
 export enum environment {
-  apiUrl = 'https://65.2.4.69/',
+  apiUrl = 'https://api.yogavidyaschool.com/',
   imageUrl = 'https://d29rwrqvux6m5p.cloudfront.net/images/',
   // apiUrl = 'http://localhost:3000/',
   // imageUrl = 'http://localhost:3000/public/images/',
